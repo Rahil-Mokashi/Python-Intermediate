d29e07f0714b6c881392ef2b5fd77eb7d85ffbdf
Rahil Mokashi <rahilmokashi07@gmail.com>
2025-08-02 09:23:14 +0530

    Added Project: Mail Merge

--- Mail Merge Project/main.py
+++ Mail Merge Project/main.py
@@ -3,6 +3,23 @@
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Names/invited_names.txt", 'r') as invited_name:
+    names = invited_name.readlines()
+
+with open("./Input/Letters/starting_letter.txt", 'r') as letter:
+    letter_template = letter.read()
+
+for name in names:  
+    actual_name = name.strip('\n')        
+    final_letter = letter_template.replace("[name]", actual_name)
+    
+    
+    with open(f"./Output/ReadyToSend/letter_for_{actual_name}.txt", 'w') as individual_letter:
+        separate_letter = individual_letter.write(final_letter)
+        
+
+
+    
+
+
